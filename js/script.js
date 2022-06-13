87e50619f04b4401fde75baf3f50a0ddd11733b3
Alexander <kanevdev@yandex.ru>
2022-06-13 18:18:56 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+a = 15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+var name = 'Ivan';
+
+{
+    var result = 50;
+}
+
+console.log(result);
